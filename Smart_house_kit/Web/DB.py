8404157924be1729a126eb7dfb416baa147cd2a2
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-10-04 08:19:41 +0900

    Add files via upload

--- Smart_house_kit/Web/DB.py
+++ Smart_house_kit/Web/DB.py
@@ -98,6 +98,7 @@ class DAO :
 
 
 ############ 커뮤니티 ################
+
     # 현재 페이지 글 목록 조회하기
     def select_content_list(now,content_type,sort_by='latest',selector='a', search='') :
 
@@ -198,3 +199,179 @@ class DAO :
          self.close_db()
 
          return redirect(url_for('community'))
+
+
+############ 마이페이지 #############
+    # 센서 현재 상태 확인
+    def sensor_condition(data) :
+        for kit in [('DHT11_HUMI',['건조', 50, '정상', 80, '과습']), ('DHT11_TEMP',['저온', 20, '정상',  30, '고온'])] :
+            try :
+                dergree = data['kit'][kit[0]]
+                if degree < kit[1][1] :
+                    data['kit'][kit[0]] = [degree,kit[1][0]]
+                elif degree < kit[1][3] :
+                    data['kit'][kit[0]] = [degree,kit[1][2]]
+                else :
+                    data['kit'][kit[0]] = [degree,kit[1][4]]
+                if data['kit'][kit[0]][-1] != '정상' :
+                    cursor.execute(f"""
+                    select (SYSDATE - t1.sv_date) AS DELAY
+                    from tbl_kit_sensoring t1
+                    inner join tbl_kit t2
+                    on t1.kit_seq=t2.kit_seq
+                    where t2.cage_seq = {data['cage_seq']} and t2.kit_name = '{kit[0]}'
+                    and t1.sv_date = 
+                    (select MAX(sv_date) from tbl_kit_sensoring where kit_seq = t1.kit_seq AND SV_VALUE > {kit[1][1]} AND SV_VALUE < {kit[1][3]})
+
+                    union all
+
+                    select (sysdate - kit_inst_date) as DELAY from tbl_kit where cage_seq = {data['cage_seq']} and kit_name = '{kit[0]}'
+                    and not exists
+                    (select (SYSDATE - t1.sv_date) AS DELAY
+                    from tbl_kit_sensoring t1
+                    inner join tbl_kit t2
+                    on t1.kit_seq=t2.kit_seq
+                    where t2.cage_seq = {data['cage_seq']} and t2.kit_name = '{kit[0]}'
+                    and t1.sv_date = 
+                    (select MAX(sv_date) from tbl_kit_sensoring where kit_seq = t1.kit_seq AND SV_VALUE > {kit[1][1]} AND SV_VALUE < {kit[1][3]}))
+                    """)
+                    data1 = cursor.fetchall()
+                    time = float(data1[0][0])
+                    data['kit'][kit[0]] += [cal_delay(time)]
+            except :
+                pass
+        for kit in ['HEATING_PAD','HUMIDIFIER','COOLING_PAN8*8','COOLING_PAN8*8'] :
+            try :
+                if int(date['kit'][kit]) == 1 :
+                    date['kit'][kit] = '켜짐'
+                elif int(date['kit'][kit]) == 0 :
+                    date['kit'][kit] = '꺼짐'
+                else :
+                    date['kit'][kit] = '에러' 
+            except :
+                pass
+        return data
+
+
+    # 사육장 목록 조회하기
+    def call_cage_list(mb_id) :  # 케이지 번호, 이름, 애완동물 종류, 애완동물 이름
+        os.putenv('NLS_LANG', '.UTF8') # 한글 지원하기
+        conn = cx_Oracle.connect('diamond','1234','project-db-stu.ddns.net:1524/xe')
+        cursor = conn.cursor()
+        cursor.execute(f"""
+            SELECT T3.CAGE_SEQ, T3.CAGE_NAME, LISTAGG(T3.PET_TYPE,'/') WITHIN GROUP (ORDER BY T3.PET_TYPE) AS PET_TYPE, LISTAGG(T3.PET_NAME,'/') WITHIN GROUP (ORDER BY T3.PET_NAME) AS PET_NAME
+            FROM (SELECT T1.CAGE_SEQ, T1.CAGE_NAME,T2.PET_TYPE, LISTAGG(T2.PET_NAME,',') WITHIN GROUP (ORDER BY T2.PET_NAME) AS PET_NAME, T1.MB_ID
+            FROM TBL_CAGE T1, TBL_PET T2
+            WHERE T1.CAGE_SEQ = T2.CAGE_SEQ
+            GROUP BY T1.CAGE_SEQ, T1.CAGE_NAME, T2.PET_TYPE, T1.MB_ID
+            ORDER BY T1.CAGE_SEQ, COUNT(T2.PET_TYPE) DESC) T3
+            WHERE T3.MB_ID = '{mb_id}'
+            GROUP BY T3.CAGE_SEQ, T3.CAGE_NAME
+        """)
+        data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        if data :
+            cage_list = []
+            for cage in data :
+                cage_info = [cage[0],cage[1]]
+                for i in range(2,4) :
+                    if len(cage[i]) > 10 :
+                        comma_index = 0
+                        slash_index = 0
+                        if ',' in cage[i][:11] :
+                            comma_index = cage[i][:11].rindex(',')
+                        if '/' in cage[i][:11] :
+                            slash_index = cage[i][:11].rindex('/')
+                        index = max(comma_index, slash_index)
+                        cage_info.append(cage[i][:index+1]+'...')
+                    else :
+                        cage_info.append(cage[i])
+                cage_list.append(cage_info)
+            return cage_list
+        else :
+            return []
+
+
+    # 사육장번호로 사육장 정보 조회하기
+    def call_cage_seq(mb_id, cage_seq) :
+        os.putenv('NLS_LANG', '.UTF8') # 한글 지원하기
+        conn = cx_Oracle.connect('diamond','1234','project-db-stu.ddns.net:1524/xe')
+        cursor = conn.cursor()
+        cursor.execute(f"select cage_name from tbl_cage where cage_seq = {cage_seq} and mb_id = '{mb_id}'")
+        data = cursor.fetchall()
+        if data != [] :
+            data = {'cage_seq': cage_seq, 'cage_name' : data[0][0]}
+            cursor.execute(f"""
+                select t1.kit_name, t2.sv_value
+                from (select t3.kit_seq, max(t3.sv_date) as sv_date, t4.kit_name 
+                from tbl_kit_sensoring t3, tbl_kit t4 
+                where t4.kit_seq=t3.kit_seq and t4.cage_seq = {cage_seq}
+                group by t3.kit_seq, t4.kit_name) t1
+                inner join tbl_kit_sensoring t2 
+                on t1.kit_seq = t2.kit_seq
+                where t1.sv_date = t2.sv_date
+            """)
+            data1 = cursor.fetchall()
+            data['kit'] = dict(data1)
+            cursor.execute(f"""
+                select distinct t3.pet_type, listagg(t3.pet_info, '\n') WITHIN GROUP (ORDER BY t3.pet_info) AS PET_INFO
+                FROM (select PET_TYPE, (PET_NAME ||'('||
+                (CASE
+                WHEN PET_GENDER = 'f' THEN '♀'
+                WHEN PET_GENDER = 'm' THEN '♂'
+                ELSE '?'
+                END)
+                ||')') as pet_info
+                from tbl_pet WHERE CAGE_SEQ=2) T3 GROUP BY T3.PET_TYPE
+                """)
+            data2 = cursor.fetchall()
+            data['pet'] = data2
+            result = sensor_condition(data)
+            cursor.close()
+            conn.close()
+            return result
+        else :
+            cursor.close()
+            conn.close()
+            return redirect(url_for('mypage_info', info='365666'))
+
+    # 사육장 상태 그래프 조회하기
+    def load_graph(today,  mb_id, cage_seq) :
+        os.putenv('NLS_LANG', '.UTF8') # 한글 지원하기
+        conn = cx_Oracle.connect('diamond','1234','project-db-stu.ddns.net:1524/xe')
+        cursor = conn.cursor()
+        cursor.execute(f"""
+            select trunc(avg(sv_value),2), to_char(median(sv_date), 'AM HH:MI') from tbl_kit_sensoring t1 inner join tbl_kit t2 on t1.kit_seq = t2.kit_seq where t2.kit_name = 'DHT11_TEMP' and to_char(sv_date, 'YYYY-MM-DD') = '{today}' GROUP BY to_char(sv_date, 'AM HH:MI')
+                """)
+
+        data = cursor.fetchall()
+        temp = [[a[0] for a in data],[a[1] for a in data]]
+
+        cursor.execute(f"""
+            select trunc(avg(sv_value),2), to_char(median(sv_date), 'AM HH:MI') from tbl_kit_sensoring t1 inner join tbl_kit t2 on t1.kit_seq = t2.kit_seq where t2.kit_name = 'DHT11_HUMI' and to_char(sv_date, 'YYYY-MM-DD') = '{today}' GROUP BY to_char(sv_date, 'AM HH:MI')
+                """)
+
+        data = cursor.fetchall()
+        humi = [[a[0] for a in data],[a[1] for a in data]]
+
+        cursor.execute(f"""
+            select trunc(avg(sv_value),2), to_char(median(sv_date), 'AM HH:MI') from tbl_kit_sensoring t1 inner join tbl_kit t2 on t1.kit_seq = t2.kit_seq where t2.kit_name = 'PAD_TEMP' and to_char(sv_date, 'YYYY-MM-DD') = '{today}' GROUP BY to_char(sv_date, 'AM HH:MI')
+                """)
+
+        data = cursor.fetchall()
+        pad_temp = [[a[0] for a in data],[a[1] for a in data]]
+
+
+        plt.figure(figsize=(20,8))
+        plt.plot(range(0, 6,1), temp[0], c='red', marker='*')
+        plt.plot(range(0, 6,1), humi[0], c='blue', marker='*')
+        plt.plot(range(0, 6,1), pad_temp[0], c='orange', marker='*')
+        plt.xticks(range(0, 6,1), labels=temp[1], rotation=45)
+        plt.xlim(0, 5)
+        plt.ylim(10, 60)
+        plt.grid()
+        plt.title(f'Today_Sensoring({today})')
+        plt.savefig(f'./static/img/{today}_{mb_id}_{cage_seq}.png')
+        cursor.close()
+        conn.close()
