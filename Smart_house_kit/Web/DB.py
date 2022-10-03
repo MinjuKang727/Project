5bef2e19890c0c211ec5b4642089953679b0d267
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-10-03 23:08:53 +0900

    Add files via upload

--- Smart_house_kit/Web/DB.py
+++ Smart_house_kit/Web/DB.py
@@ -25,12 +25,13 @@ class DAO :
             if curs != None :
                 curs.close() # cursor 통로 닫기
         except :
-            pass
+            print('cursor 종료 실패')
+
         try :
             if conn != None :
                 conn.close() # Connection 통로 닫기    
         except :
-            pass
+            print('connection 종료 실패')
         
 ##################### 로그인 ######################
     # 로그인 하기
@@ -78,17 +79,122 @@ class DAO :
             return render_template('/404')
 
     # ID 중복 검사
-    def id_ck(ID) :
+    def id_ck(mb_id) :
         result = False
         conn, curs = self.conn_db()  # DB 접속
 
-        cursor.execute("""SELECT count(*) FROM tbl_member 
-                        where mb_id= :1 """)
+        sql = """SELECT count(*) FROM tbl_member 
+                        where mb_id= :1 """
+        curs.execute(sql, [mb_id])
         data = cursor.fetchone()
+
         self.close_db()  # DB 접속 종료
 
-        if data:  # 조회 결과가 있으면
+        if len(data):  # 조회 결과가 있으면
             if data[0] == 0 :
                 result = True
 
         return result
+
+
+############ 커뮤니티 ################
+    # 현재 페이지 글 목록 조회하기
+    def select_content_list(now,content_type,sort_by='latest',selector='a', search='') :
+
+        conn, curs = self.conn_db()  # DB 접속
+        
+        params = [content_type]
+    
+        if selector == 't' :
+           query = f"t1.commu_title like '%{search}%'"
+        elif selector == 'w' :
+            query = f"t1.mb_id like '%{search}%'"
+        elif selector == "c" :
+           query = f"t1.commu_content like '%{search}%'"
+        else :
+            query = f"(t1.commu_title like '%{search}%' or t1.commu_content like '%{search}%' or t1.mb_id like '%{search}%')"
+       params += [query] 
+       
+        if sort_by == 'likes' :
+            params += ['commu_like']
+        elif sort_by == 'views' :
+            params += ['commu_views']
+        else :
+            params += ['commu_update2']
+        params += [now, now]
+
+        sql ="""select *  from 
+            (select commu_seq, rownum as commu_index, commu_title, mb_id, commu_update, commu_views, commu_like
+            from (select t1.commu_seq, t1.commu_title, t1.mb_id, t1.commu_like, t1.commu_views, t1.commu_update, to_char(t1.commu_update, 'YYYY-MM-DD HH24:MI:SS') as commu_update2 
+            from tbl_community t1 where t1.commu_type=:1 and :2 order by :3 desc,t1.commu_seq desc))
+            where commu_index > (:4 - 1)*20 and commu_index <= :5 * 20"""
+
+        curs.execute(sql, params)
+        content_list = curs.fetchall()
+
+        self.close_db()  # DB 접속 종료
+
+        return content_list
+
+
+    # 총 게시글 수 조회하기
+    def contents_total(content_type) :
+
+        conn, curs = self.conn_db()  # DB 접속
+
+        sql = """select ceil(count(*)/20) from tbl_community where commu_type = :1"""
+
+        curs.execute(sql, [content_type])
+        data = curs.fetchone()
+
+        if len(data) :
+	total = int(data[0])
+        else :
+	total = 0
+    
+        self.close_db()  # DB 접속 종료
+
+        return total
+
+    # 게시글번호로 게시글 조회하기
+    def call_content(commu_seq) :
+
+        conn, curs = self.conn_db()  # DB 접속
+
+        sql = """select commu_title, commu_content, mb_id, commu_update, commu_like, commu_views, commu_seq from tbl_community where commu_seq= :1"""
+        curs.execute(sql, [commu_seq])
+
+        data = cursor.fetchone()
+        if len(data) :
+            data = data[0]
+        else :
+            data = ()
+       
+        self.close_db()  # DB 접속 종료
+
+        return data
+
+
+    # 조회수 업데이트
+    def update_view(commu_seq) :
+        conn, curs = self.conn_db()  # DB 접속
+
+        sql = "update tbl_community set commu_views = commu_views + 1 where commu_seq = :1"
+        curs.execute(sql, [commu_seq])
+        conn.commit()
+
+        self.close_db()  # DB 접속 종료
+    
+
+    # 게시글 수정하기
+    def update_content(commu_seq, commu_title, commu_content) :
+         conn, curs = self.conn_db()  # DB 접속
+
+         sql = "update tbl_community set (commu_title, commu_content, commu_update) = ( :1 , :2 ,sysdate) where commu_seq = :3"
+   
+         curs.execute(sql, [commu_title, commu_content, commu_seq])
+         conn.commit()
+
+         self.close_db()
+
+         return redirect(url_for('community'))
