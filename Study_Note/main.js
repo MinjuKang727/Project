ffab3c0e21d8041abf9c1d556bb04732433611ba
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-10-17 01:24:24 +0900

    Add files via upload

---
+++ Study_Note/main.js
@@ -0,0 +1,8 @@
+$('#study').click(()=>{
+   $('#study').hide();
+   $('#on').show();
+});
+// let today = new Date();
+//    set
+//    $('#now').text(today.toLocaleString());
+console.log($('#on'.display();));
