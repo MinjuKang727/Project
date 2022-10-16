c8c91b2cd5a299978674e555b6bc2dac2209084b
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-10-17 01:25:20 +0900

    Delete main.js

--- Study_Note/main.js
+++
@@ -1,8 +0,0 @@
-$('#study').click(()=>{
-   $('#study').hide();
-   $('#on').show();
-});
-// let today = new Date();
-//    set
-//    $('#now').text(today.toLocaleString());
-console.log($('#on'.display();));
