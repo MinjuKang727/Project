024ff7cbe3cde9dad8c79eda262c0b21f841ec7b
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-12-08 02:50:14 +0900

    Add files via upload

--- 2차 프로젝트/자율주행/static/test.js
+++ 2차 프로젝트/자율주행/static/test.js
@@ -1,52 +1,91 @@
 function uploadIMG(){
-  let formData = $('form[name="area"]').serialize();
-  let a_name = $('input[name="A_NAME"]').val();
-  let file_name = $('input[name="file"]').val();
-  if (a_name != "" && file_name != "") {
+  let formData = new FormData();
+  formData.append('A_NAME', $('input[name=A_NAME]').val());
+  formData.append('file', $('input[name=file]')[0].files[0]);
+  let validCSS = {
+    'height' : '30px',
+    'color' : 'green',
+    'font-size' : '15px'
+  }
+  let invalidCSS = {
+    'height' : '30px',
+    'color' : 'red',
+    'font-size' : '15px'
+  }
+  let check = true;
+  if ($('input[name=A_NAME]').val() == ""){
+    console.log('name == ""');
+    $('.A_NAME.feedback').css(invalidCSS);
+    $('.A_NAME.feedback').text('공간 이름을 입력해 주십시오.');
+    check = false;
+  }else{
+    $('.A_NAME.feedback').css('height', "0");
+    $('.A_NAME.feedback').text("");
+  } 
+  if ($('input[name=file]').val() == ""){
+    console.log('file == ""');
+    $('.A_SRC.feedback').css(invalidCSS);
+    $('.A_SRC.feedback').text('설계도면 이미지 파일을 선택해 주십시오.');
+    check = false;
+  }else{
+    $('.A_SRC.feedback').css('height', "0");
+    $('.A_SRC.feedback').text("");
+  }
+  if (check){
     $.ajax({
         url : "/set/map/upload_result", 
         type : 'POST', 
         data : formData, 
-    }).then((result)=>{
-      console.log(result);
+        contentType: false,
+        processData: false
+    }).done((result)=>{
       if (result["result"]){
-        $('#A_SRC > .feedback').attr('class', "feedback valid-feedback");
-        $('#A_SRC > .feedback').text(result["message"]);
+        $('.A_SRC.feedback').css(validCSS);
+        $('.A_SRC.feedback').text(result["message"]);
+        floatMap(result["map_src"]);
       }else{
-        $('#A_SRC > .feedback').attr('class', "feedback invalid-feedback");
-        $('#A_SRC > .feedback').text(result["message"]);
+        $('.A_SRC.feedback').css(invalidCSS);
+        $('.A_SRC.feedback').text(result["message"]);
       }
     });
   }
 };
 
-// let path = '';
-// let plan = function( x, y, z ) {
-//     y = -y - 1;
-//     let limit = Math.ceil( 3 / Math.pow( 2, z ) );
+function floatMap(map_src){
+  console.log('map_src');
+  var path = './static/upload/';
+  var plan = function( x, y, z ) {
+      y = -y - 1;
+      var limit = Math.ceil( 3 / Math.pow( 2, z ) );
 
-//     if ( 0 <= y && y < limit && 0 <= x && x < limit ) {
-//         return domain + path + 'planh' +
-//             z + '_' + y + '_' + x + '.png';
-//     } else {
-//         return 'https://i1.daumcdn.net/dmaps/apis/white.png';
-//     }
-// };
+      if ( 0 <= y && y < limit && 0 <= x && x < limit ) {
+          return path + map_src;
+      } else {
+          return 'https://i1.daumcdn.net/dmaps/apis/white.png';
+      }
+  };
 
-// kakao.maps.Tileset.add( 'PLAN',
-//         new kakao.maps.Tileset(
-//             512, 512, plan, [], false, 0, 2) );
+  kakao.maps.Tileset.add( 'PLAN',
+          new kakao.maps.Tileset(
+              512, 512, plan, '', false, 0, 8 ) );
 
-// let node = document.getElementById( 'map' );
-// let map = new kakao.maps.Map( node, {
-//     projectionId: null,
-//     mapTypeId: kakao.maps.MapTypeId.PLAN,
-//     $scale: false,
-//     center: new kakao.maps.Coords( 650, -550 ),
-//     level: 2
-// } );
-// let center = map.getCenter();
-// let marker = new kakao.maps.Marker({
-//     position: center
-// });
-// marker.setMap(map);
+  var node = document.getElementById( 'map' );
+  var map = new kakao.maps.Map( node, {
+      projectionId: null,
+      mapTypeId: kakao.maps.MapTypeId.PLAN,
+      $scale: false,
+      center: new kakao.maps.Coords( 650, -550 ),
+      level: 4
+  } );
+  var center = map.getCenter();
+  var marker = new kakao.maps.Marker({
+      position: center
+  });
+  marker.setMap(map);
+
+  var infowindow = new kakao.maps.InfoWindow({
+      content: '커스텀 타일셋을 올릴수 있습니다!'
+  });
+  infowindow.open(map, marker);
+
+}
