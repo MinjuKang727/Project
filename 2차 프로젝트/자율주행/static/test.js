ba39d01d852df241c727bfdc0ece53a23abd68a6
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-12-09 00:18:44 +0900

    Add files via upload

--- 2차 프로젝트/자율주행/static/test.js
+++ 2차 프로젝트/자율주행/static/test.js
@@ -2,6 +2,7 @@ function uploadIMG(){
   let formData = new FormData();
   formData.append('A_NAME', $('input[name=A_NAME]').val());
   formData.append('file', $('input[name=file]')[0].files[0]);
+  console.log(formData.get('file'));
   let validCSS = {
     'height' : '30px',
     'color' : 'green',
@@ -32,6 +33,7 @@ function uploadIMG(){
     $('.A_SRC.feedback').text("");
   }
   if (check){
+    ResizeImage();
     $.ajax({
         url : "/set/map/upload_result", 
         type : 'POST', 
@@ -42,7 +44,7 @@ function uploadIMG(){
       if (result["result"]){
         $('.A_SRC.feedback').css(validCSS);
         $('.A_SRC.feedback').text(result["message"]);
-        floatMap(result["map_src"]);
+        // floatMap(result["map_src"]);
       }else{
         $('.A_SRC.feedback').css(invalidCSS);
         $('.A_SRC.feedback').text(result["message"]);
@@ -51,41 +53,193 @@ function uploadIMG(){
   }
 };
 
-function floatMap(map_src){
-  console.log('map_src');
-  var path = './static/upload/';
-  var plan = function( x, y, z ) {
-      y = -y - 1;
-      var limit = Math.ceil( 3 / Math.pow( 2, z ) );
+// function floatMap(map_src){
+//   console.log('map_src');
+//   let path = './static/upload/';
+//   let plan = function( x, y, z ) {
+//       y = -y - 1;
+//       let limit = Math.ceil( 3 / Math.pow( 2, z ) );
 
-      if ( 0 <= y && y < limit && 0 <= x && x < limit ) {
-          return path + map_src;
-      } else {
-          return 'https://i1.daumcdn.net/dmaps/apis/white.png';
+//       if ( 0 <= y && y < limit && 0 <= x && x < limit ) {
+//           return path + map_src;
+//       } else {
+//           return 'https://i1.daumcdn.net/dmaps/apis/white.png';
+//       }
+//   };
+
+//   kakao.maps.Tileset.add( 'PLAN',
+//           new kakao.maps.Tileset(
+//               512, 512, plan, '', false, 0, 8 ) );
+
+//   let node = document.getElementById( 'map' );
+//   let map = new kakao.maps.Map( node, {
+//       projectionId: null,
+//       mapTypeId: kakao.maps.MapTypeId.PLAN,
+//       $scale: false,
+//       center: new kakao.maps.Coords( 650, -550 ),
+//       level: 4
+//   } );
+//   let center = map.getCenter();
+//   let marker = new kakao.maps.Marker({
+//       position: center
+//   });
+//   marker.setMap(map);
+
+//   let infowindow = new kakao.maps.InfoWindow({
+//       content: '커스텀 타일셋을 올릴수 있습니다!'
+//   });
+//   infowindow.open(map, marker);
+
+// }
+
+// function ResizeImage() {
+
+// let filesToUpload = document.getElementById('imageFile').files;
+
+// let file = filesToUpload[0];
+
+ 
+
+// // 문서내에 img 객체를 생성합니다
+
+// let img = document.createElement("img");
+
+// // 파일을 읽을 수 있는 File Reader 를 생성합니다
+
+// let reader = new FileReader();
+
+ 
+
+//     // 파일이 읽혀지면 아래 함수가 실행됩니다
+
+// reader.onload = function(e) {
+
+//         img.src = e.target.result;
+
+ 
+
+// // HTML5 canvas 객체를 생성합니다
+
+//         let canvas = document.createElement("canvas");      
+
+//         let ctx = canvas.getContext("2d");
+
+ 
+
+//         // 캔버스에 업로드된 이미지를 그려줍니다
+
+//         ctx.drawImage(img, 0, 0);
+
+ 
+
+//         // 최대폭을 400 으로 정했다고 가정했을때
+
+//         // 최대폭을 넘어가는 경우 canvas 크기를 변경해 줍니다.
+
+//         let MAX_WIDTH = 400;
+
+//         let MAX_HEIGHT = 400;
+
+//         let width = img.width;
+
+//         let height = img.height;
+
+ 
+
+//         if (width > height) {
+
+//             if (width > MAX_WIDTH) {
+
+//                 height *= MAX_WIDTH / width;
+
+//                 width = MAX_WIDTH;
+
+//             }
+
+//         } else {
+
+//             if (height > MAX_HEIGHT) {
+
+//                 width *= MAX_HEIGHT / height;
+
+//                 height = MAX_HEIGHT;
+
+//             }
+
+//         }
+
+//         canvas.width = width;
+
+//         canvas.height = height;
+
+ 
+
+//         // canvas에 변경된 크기의 이미지를 다시 그려줍니다.
+
+//         let ctx = canvas.getContext("2d");
+
+//         ctx.drawImage(img, 0, 0, width, height);
+
+ 
+
+// // canvas 에 있는 이미지를 img 태그로 넣어줍니다
+
+//         let dataurl = canvas.toDataURL("image/png");
+
+//         document.getElementById('output').src = dataurl;
+
+//     }
+
+// reader.readAsDataURL(file);
+
+// }
+
+
+function ResizeImage() {
+  let file = $('input[name=file]')[0].files[0];
+  let img = $('img');
+  // 파일을 읽을 수 있는 File Reader 를 생성합니다
+  let reader = new FileReader();
+  
+      // 파일이 읽혀지면 아래 함수가 실행됩니다
+  reader.onload = function(e) {
+          img.src = e.target.result;
+  // HTML5 canvas 객체를 생성합니다
+          let canvas = document.createElement("canvas");      
+          let ctx = canvas.getContext("2d");
+  
+          // 캔버스에 업로드된 이미지를 그려줍니다
+          ctx.drawImage(img, 0, 0);
+   
+          // 최대폭을 400 으로 정했다고 가정했을때
+          // 최대폭을 넘어가는 경우 canvas 크기를 변경해 줍니다.
+  
+          let MAX_WIDTH = 400;
+          let MAX_HEIGHT = 400;
+          let width = img.width;
+          let height = img.height;
+   
+          if (width > height) {
+              if (width > MAX_WIDTH) {
+                  height *= MAX_WIDTH / width;
+                  width = MAX_WIDTH;
+              }
+          } else {
+              if (height > MAX_HEIGHT) {
+                  width *= MAX_HEIGHT / height;
+                  height = MAX_HEIGHT;
+              }
+          }
+          canvas.width = width;
+          canvas.height = height;
+  
+          // canvas에 변경된 크기의 이미지를 다시 그려줍니다.
+          ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0, width, height);
+
+  // canvas 에 있는 이미지를 img 태그로 넣어줍니다
+          let dataurl = canvas.toDataURL("image/png");
+          $('img').attr('src', dataurl);
       }
-  };
-
-  kakao.maps.Tileset.add( 'PLAN',
-          new kakao.maps.Tileset(
-              512, 512, plan, '', false, 0, 8 ) );
-
-  var node = document.getElementById( 'map' );
-  var map = new kakao.maps.Map( node, {
-      projectionId: null,
-      mapTypeId: kakao.maps.MapTypeId.PLAN,
-      $scale: false,
-      center: new kakao.maps.Coords( 650, -550 ),
-      level: 4
-  } );
-  var center = map.getCenter();
-  var marker = new kakao.maps.Marker({
-      position: center
-  });
-  marker.setMap(map);
-
-  var infowindow = new kakao.maps.InfoWindow({
-      content: '커스텀 타일셋을 올릴수 있습니다!'
-  });
-  infowindow.open(map, marker);
-
-}
+  reader.readAsDataURL(file);
+  }
