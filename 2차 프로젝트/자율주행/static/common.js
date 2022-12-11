41cfa0906f556411b5b52fd33b70c28c0e9544e3
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2022-12-12 02:20:44 +0900

    Add files via upload

--- 2차 프로젝트/자율주행/static/common.js
+++ 2차 프로젝트/자율주행/static/common.js
@@ -297,3 +297,85 @@ let content_ck = () => {
   }
 });
 }
+
+
+///////////// 관리 및 설정 ////////////////
+function uploadIMG(){
+  let formData = new FormData();
+  formData.append('A_NAME', $('input[name=A_NAME]').val());
+  formData.append('file', $('input[name=file]')[0].files[0]);
+
+  let validCSS = {
+    'height' : '30px',
+    'color' : 'green',
+    'font-size' : '15px'
+  }
+  let invalidCSS = {
+    'height' : '30px',
+    'color' : 'red',
+    'font-size' : '15px'
+  }
+  let check = true;
+  if ($('input[name=A_NAME]').val() == ""){
+    console.log('name == ""');
+    $('.A_NAME.feedback').css(invalidCSS);
+    $('.A_NAME.feedback').text('공간 이름을 입력해 주십시오.');
+    check = false;
+  }else{
+    $('.A_NAME.feedback').css('height', "0");
+    $('.A_NAME.feedback').text("");
+  } 
+  if ($('input[name=file]').val() == ""){
+    $('img').attr('src', "");
+    $('.A_SRC.feedback').css(invalidCSS);
+    $('.A_SRC.feedback').text('설계도면 이미지 파일을 선택해 주십시오.');
+    check = false;
+  }else{
+    $('.A_SRC.feedback').css('height', "0");
+    $('.A_SRC.feedback').text("");
+    $('img').attr('src', "");
+  }
+  if (check){
+  
+    $.ajax({
+    url : "/set/map/upload_result", 
+    type : 'POST', 
+    data : formData, 
+    contentType: false,
+    processData: false
+    }).done((result)=>{
+    if (result["result"]){
+      $('.A_SRC.feedback').css(validCSS);
+      $('.A_SRC.feedback').text(result["message"]);
+      $('input[name=fname]').val(result["fname"]);
+
+    }else{
+      $('.A_SRC.feedback').css(invalidCSS);
+      $('.A_SRC.feedback').text(result["message"]);
+    }
+  });
+  }
+};
+
+function previewIMG(rotation){
+  let file = $('input[name=file]')[0].files[0];
+  let angle = Number($('input[name=angle]').val());
+  if (rotation == 90){
+    angle = (angle + rotation) % 360;
+    $('input[name=angle]').val(angle);
+  }
+  $('img').attr('src', resizeIMG(file, angle));
+}
+
+function resizeIMG(file, angle){
+  $.canvasResize(file, {
+    width: 300,
+    height: 0,
+    crop: false,
+    quality: 100,
+    rotate: angle,
+    callback: function(data, width, height) {
+        return data
+    }
+});
+}
